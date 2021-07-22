9be27725eeed8c00b65fd8b440b8e1967b577a35
Jackodi <lisi879@gmail.com>
2021-07-21 22:13:39 -0500

    MedicamentosActivity, recetas direcciona a medicamentos

--- app/src/main/java/com/example/appssm/RecetasActivity.java
+++ app/src/main/java/com/example/appssm/RecetasActivity.java
@@ -2,16 +2,35 @@ package com.example.appssm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.Objects;
 
 public class RecetasActivity extends AppCompatActivity {
+    Button btn_med;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recetas);
         Objects.requireNonNull(getSupportActionBar()).hide();
+
+        btn_med = (Button) findViewById(R.id.btn_med);
+        btn_med.setOnClickListener(v ->
+                ejemlo()
+        );
+    }
+
+    private void ejemlo() {
+
+            Intent intent = new Intent(RecetasActivity.this, MedicamentosActivity.class);
+            startActivity(intent);
+            Toast.makeText(this, "Activity de Medicamentos", Toast.LENGTH_SHORT).show();
+            finish();
+
     }
 }

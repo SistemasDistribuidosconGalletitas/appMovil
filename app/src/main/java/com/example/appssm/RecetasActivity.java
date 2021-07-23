dffa001c0d30259db3e275993e57ed2a0b8dcb41
Jackodi <lisi879@gmail.com>
2021-07-23 17:33:13 -0500

    Algunos cambios

--- app/src/main/java/com/example/appssm/RecetasActivity.java
+++ app/src/main/java/com/example/appssm/RecetasActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -26,11 +27,11 @@ public class RecetasActivity extends AppCompatActivity {
 //        );
     }
 
-//    private void ejemlo() {
-//
-//            Intent intent = new Intent(RecetasActivity.this, MedicamentosActivity.class);
-//            startActivity(intent);
-//            Toast.makeText(this, "Activity de Medicamentos", Toast.LENGTH_SHORT).show();
-//            //finish();
-//    }
+    public void ejemplo(View v) {
+
+            Intent intent = new Intent(RecetasActivity.this, MedicamentosActivity.class);
+            startActivity(intent);
+            Toast.makeText(this, "Activity de Medicamentos", Toast.LENGTH_SHORT).show();
+            //finish();
+    }
 }

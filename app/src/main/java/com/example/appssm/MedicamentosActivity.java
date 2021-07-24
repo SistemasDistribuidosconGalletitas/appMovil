5f3953f6a3fe669c3158a5835a06329236290b34
Jackodi <lisi879@gmail.com>
2021-07-23 20:58:31 -0500

    Ejemplo de notificacion

--- app/src/main/java/com/example/appssm/MedicamentosActivity.java
+++ app/src/main/java/com/example/appssm/MedicamentosActivity.java
@@ -1,9 +1,15 @@
 package com.example.appssm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import java.util.Objects;
@@ -11,6 +17,7 @@ import java.util.Objects;
 public class MedicamentosActivity extends AppCompatActivity {
 
     ImageView img_receta;
+    ImageView img_noti;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +29,27 @@ public class MedicamentosActivity extends AppCompatActivity {
         img_receta.setOnClickListener(v ->
                 regresarReceta()
         );
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("CHANNEL_ID","CHANNEL_ID", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
     }
     private void regresarReceta(){
         Intent intent = new Intent(MedicamentosActivity.this, RecetasActivity.class);
         startActivity(intent);
         //finish();
 
+    }
+
+    public void sendNotification(View view){
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, "CHANNEL_ID")
+                .setSmallIcon(R.drawable.icono_med1)
+                .setContentTitle("Naproxeno Sódico")
+                .setContentText("Debes de tomar este medicamento a las 12:00 pm");
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(1,mBuilder.build());
     }
 }
